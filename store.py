08ddf51816d1d30954284bd1573ad29d145509aa
Maya Ron <mayaron31@gmail.com>
2024-01-23 00:17:13 +0200

    ffvygyugy

--- store.py
+++ store.py
@@ -3,6 +3,8 @@ import yaml
 from item import Item
 from shopping_cart import ShoppingCart
 
+import errors as errors_module
+
 class Store:
     """
     Consturctor which takes a path to an items file and loads it into the items field of the class
@@ -11,8 +13,7 @@ class Store:
         with open(path) as inventory:
             items_raw = yaml.load(inventory, Loader=yaml.FullLoader)['items']
         self._items = self._convert_to_item_objects(items_raw)
-        self._shopping_cart = ShoppingCart()
-
+        self._shopping_cart = ShoppingCart() 
 
     """
     transforms raw item data into item objects
@@ -37,7 +38,6 @@ class Store:
     def get_items(self) -> list:
         return self._items
 
-
     """
     Searches for item in list
     Arguments:
@@ -50,10 +50,10 @@ class Store:
     def search_by_name(self, item_name: str) -> list:
         fitting_items = [] # initialize a list which will contain the fitting items that will be returned
         for item in self._items:
-            if item.name == item_name:
+            if item_name in item.name:
                 fitting_items.append(item)
 
-        return sorted(fitting_items, key=lambda item: item.name) # return the fitting items list sorted , by their names      
+        return sorted(fitting_items, key=lambda item: item.name) # return the fitting items list sorted , by their names  #TODO: learn fuzzy matching    
 
   
     """
@@ -67,19 +67,37 @@ class Store:
     def search_by_hashtag(self, hashtag: str) -> list:
         fitting_items = [] # initialize a list which will contain the fitting items that will be returned
         for item in self._items:
-            if item.hashtag == hashtag:
+            if hashtag in item.hashtags:
                 fitting_items.append(item) # add the hashtag to the fitting items
 
         return sorted(fitting_items, key=lambda item: item.name) # return the fitting items list sorted , by their names      
 
+    """
+    Adds an item with the given name to the customer’s shopping cart.
+    Arguments: the current instance of Store and an instance of str.
+    Exceptions: if no such item exists, raises ItemNotExistError. If there are multiple items matching the given name, raises TooManyMatchesError. If the given item is already in the shopping cart, raises ItemAlreadyExistsError.
+    To ease the search for the customers, not the whole item’s name must be given, but rather a distinct substring. For example, when adding "soap" to the cart, if an item such as "body soap" exists, and no other item with the substring "soap" in its name, "body soap" should be added to the list.
+    You may assume that no two items exist such that one's name is a substring of the other.
+    """
     def add_item(self, item_name: str):
-        # TODO: Complete
-        pass
+        for item in self._items:
+            if item_name == item.name:
+                self._shopping_cart.add_item(item)
+                return
+        raise errors_module.ItemNotExistError
 
+        
+
+    """
+    Removes an item with the given name from the customer’s shopping cart.
+    Arguments: the current instance of Store and an instance of str.#TODO: לכתוב כמו את השאר
+    Exceptions: if no such item exists, raises ItemNotExistError. If there are multiple items matching the given name, raises TooManyMatchesError.
+    In a similar fashion to add_item, here too, not the whole item’s name must be given for it to be removed.
+    """
     def remove_item(self, item_name: str):
-        # TODO: Complete
-        pass
-
+        self._shopping_cart.remove_item(item_name)
+    """
+    checkout(self) – Returns the total price of all the items in the costumer’s shopping cart.
+    """
     def checkout(self) -> int:
-        # TODO: Complete
-        pass
+        return self._shopping_cart.get_subtotal()

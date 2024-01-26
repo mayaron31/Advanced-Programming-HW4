d4a9483426d8384c006f3334a7040fa265c54d79
Maya Ron <mayaron31@gmail.com>
2024-01-27 00:09:24 +0200

    version 2.3

--- shopping_cart.py
+++ shopping_cart.py
@@ -3,36 +3,39 @@ import errors as errors_module
 
 
 class ShoppingCart:
-    """
-    hold itemList
-    """
+
     def __init__(self) -> None:
+        """
+        hold itemList
+        """
         self.itemList = []
 
 
-    """
-    adds the given item to the shopping cart.
 
-    Arguments:
-    item: an instance of Item.
-    Exceptions:
-    if the item name already exists in the shopping cart, raises ItemAlreadyExistsError.
-    """
     def add_item(self, item: Item):
+        """
+        adds the given item to the shopping cart.
+
+        Args:
+            item: an instance of Item.
+        Exceptions:
+            if the item name already exists in the shopping cart, raises ItemAlreadyExistsError.
+        """
         for _item in self.itemList:
             if _item.name == item.name:
                 raise errors_module.ItemAlreadyExistsError
         self.itemList.append(item) # adds item to shopping cart if needed
 
-    """
-    Removes the item with the given name from the shopping cart
-
-    Arguments:
-    item_name: a str represents the attribute name of an Item.
-    Exceptions:
-    if no item with the given name exists, raises ItemNotExistError.
-    """
+  
     def remove_item(self, item_name: str):
+        """
+        Removes the item with the given name from the shopping cart
+
+        Args:
+            item_name(str): represents the attribute name of an Item.
+        Exceptions:
+            if no item with the given name exists, raises ItemNotExistError.
+        """
         for index, item in enumerate(self.itemList): #iterate over pairs of indexes and items 
             if item.name == item_name: #check if item name is identical to what we want to remove
                 self.itemList.pop(index)
@@ -41,19 +44,26 @@ class ShoppingCart:
         raise errors_module.ItemNotExistError
 
 
-    """
-    Returns the subtotal price of all the items currently in the shopping cart.
-    """
+    
     def get_subtotal(self) -> int:
+        """
+        Returns the subtotal price of all the items currently in the shopping cart.
+        """
         subtotal = 0 # var that holds subtotal
         for item in self.itemList:
             subtotal+= item.price
         return subtotal
 
-    """"""
-        
-
+     
     def in_cart(self, item_name: str) -> bool:
+        """
+        Returns True if an item is in the cart
+        
+        Args:
+            item_name (str): name of the item
+        Returns:
+            bool: True if an item is in the cart
+        """
         for item in self.itemList:
             if item.name == item_name:
                 return True
--- store.py
+++ store.py
@@ -6,18 +6,34 @@ from shopping_cart import ShoppingCart
 import errors as errors_module
 
 class Store:
-    """
-    Consturctor which takes a path to an items file and loads it into the items field of the class
-    """
     def __init__(self, path):
+        """
+        Initialize the object with the provided path to the inventory file.
+
+        Args:
+            path (str): The path to the inventory file.
+
+        Returns:
+            None
+        """
         with open(path) as inventory:
-            items_raw = yaml.load(inventory, Loader=yaml.FullLoader)['items']
+            items_raw = yaml.load(inventory, Loader=yaml.FullLoader)['items'] # load items from inventory file
         self._items = self._convert_to_item_objects(items_raw)
         self._shopping_cart = ShoppingCart() 
 
 
     """"""
     def _sorted_items(self, items:list[Item]) -> list:
+        """
+        Sorts a list of items based on the number of common hashtags with items in the shopping cart.
+
+        Args:
+        items (list[Item]): A list of Item objects to be sorted.
+
+        Returns:
+        list: A sorted list of items. The sorting is based on the number of common hashtags with items in the shopping cart, in descending order. If two items have the same number of common hashtags, they are sorted in lexicographic order based on their names.
+
+        """
         items_from_shopping_cart = self._shopping_cart.itemList
         tags_from_shopping_cart = [hashtag for item in items_from_shopping_cart for hashtag in item.hashtags]#flattend list of all hashtags in items from shopping cart
 
@@ -32,70 +48,75 @@ class Store:
             
             item_tags_amount.append((item, count))
 
-        #for item, count in item_tags_amount:
-            
         
-        return [item[0] for item in sorted(item_tags_amount, key=lambda x: (-x[1],x[0].name))]
+        return [item[0] for item in sorted(item_tags_amount, key=lambda x: (-x[1],x[0].name))] # the minus sign is to reverse only the tags amount, but left the lexo order the same
         
 
-
-    """
-    transforms raw item data into item objects
-    Arguments:
-    item_raw (List): raw item data in a list of dictionaries
-    Returns:
-    List: list of item objects
-    """
     @staticmethod
     def _convert_to_item_objects(items_raw):
+        """
+        Convert the given list of raw item data into a list of Item objects.
+
+        Args:
+            items_raw (list): A list of raw item data, where each item is a dictionary
+                containing the keys 'name', 'price', 'hashtags', and 'description'.
+
+        Returns:
+            list: A list of Item objects created from the raw item data.
+        """
         return [Item(item['name'],
                      int(item['price']),
                      item['hashtags'],
                      item['description'])
                 for item in items_raw]
 
-    """
-    returns current list of items
-    Return:
-    List: a list of items
-    """
     def get_items(self) -> list:
+        """
+        Return the list of items.
+        """
         return self._items
 
-    """
-    Searches for item in list
-    Arguments:
-    item_name (str): instance of str to search for
-    Returns:
-    List: a sorted list of all the items that match the search term.
-    The items in the returned list must contain the given phrase and not exactly match it.
-    For example, when searching for "soap", items such as "dish soap" and "body soap" should be returned.
-    """
     def search_by_name(self, item_name: str) -> list:
+        """
+        Searches for item in list
+        Args:
+            item_name (str): instance of str to search for
+        Returns:
+            List: a sorted list of all the items that match the search term.
+            The items in the returned list must contain the given phrase and not exactly match it.
+            For example, when searching for "soap", items such as "dish soap" and "body soap" should be returned.
+        """
+
         fitting_items = [item for item in self._items if item_name in item.name and not self._shopping_cart.in_cart(item.name)] # create a list containing items that their name contains the item_name that we have searched for
         return self._sorted_items(fitting_items) # return the fitting items list sorted as the assignment declares
 
   
-    """
-    Searches items by their hashtags
-    Arguments: 
-    hashtag (str): the current instance of Store and an instance of str.
-    Returns:
-    List: a sorted list of all the items matching the search criterion. 
-    The items in the returned list must have the given hashtag in their hashtag list. 
-    For example, when searching for the hashtag "paper", items with hashtags such as "tissue paper" must not be returned."""
     def search_by_hashtag(self, hashtag: str) -> list:
+        """
+        Search for items by hashtag.
+
+        Args:
+            hashtag (str): The hashtag to search for.
+        
+        Returns:
+            list: A list of fitting items sorted by some criteria.
+        """
         fitting_items = [item for item in self._items if hashtag in item.hashtags and not self._shopping_cart.in_cart(item.name)] # create a list containing items that their hashtags contains the hashtag that we have searched for
         return self._sorted_items(fitting_items) # return the fitting items list sorted as the assignment declares       
 
-    """
-    Adds an item with the given name to the customer’s shopping cart.
-    Arguments: the current instance of Store and an instance of str.
-    Exceptions: if no such item exists, raises ItemNotExistError. If there are multiple items matching the given name, raises TooManyMatchesError. If the given item is already in the shopping cart, raises ItemAlreadyExistsError.
-    To ease the search for the customers, not the whole item’s name must be given, but rather a distinct substring. For example, when adding "soap" to the cart, if an item such as "body soap" exists, and no other item with the substring "soap" in its name, "body soap" should be added to the list.
-    You may assume that no two items exist such that one's name is a substring of the other.
-    """
+
     def add_item(self, item_name: str):
+        """
+        Add an item to the shopping cart if it exists in the store and is not already in the cart.
+
+        Args:
+            item_name (str): The name of the item to be added.
+
+        Raises:
+            TooManyMatchesError: If there are multiple matching items in the store.
+            ItemAlreadyExistsError: If the item is already in the shopping cart.
+            ItemNotExistError: If the item does not exist in the store.
+        """
         fitting_items = [item for item in self._items if item_name in item.name] # search for items with matching name in store BUT not using the search by name because it returns only items that are not in the shopping cart already
         if fitting_items:
             if len(fitting_items) > 1: # more than 1 in store
@@ -107,14 +128,17 @@ class Store:
         else:
             raise errors_module.ItemNotExistError 
         
-
-    """
-    Removes an item with the given name from the customer’s shopping cart.
-    Arguments: the current instance of Store and an instance of str.#TODO: לכתוב כמו את השאר
-    Exceptions: if no such item exists, raises ItemNotExistError. If there are multiple items matching the given name, raises TooManyMatchesError.
-    In a similar fashion to add_item, here too, not the whole item’s name must be given for it to be removed.
-    """
     def remove_item(self, item_name: str):
+        """
+        Removes the specified item from the shopping cart.
+
+        Args:
+            item_name (str): The name of the item to be removed from the shopping cart.
+
+        Raises:
+            TooManyMatchesError: If there are more than one matching items in the cart.
+            ItemNotExistError: If the specified item does not exist in the shopping cart.
+        """
         fitting_items_in_cart:list[Item] = [item for item in self._shopping_cart.itemList if item_name in item.name] # create a list of matching items in the cart
 
         if fitting_items_in_cart:
@@ -124,9 +148,13 @@ class Store:
                 self._shopping_cart.remove_item(fitting_items_in_cart[0].name) # remove item from shopping cart
         else:
             raise errors_module.ItemNotExistError 
-    """
-    checkout(self) – Returns the total price of all the items in the costumer’s shopping cart.
-    """
+
     def checkout(self) -> int:
+        """
+        Return the subtotal of the shopping cart.
+
+        Returns:
+            int: The subtotal of the shopping cart.
+        """
         return self._shopping_cart.get_subtotal()
     
